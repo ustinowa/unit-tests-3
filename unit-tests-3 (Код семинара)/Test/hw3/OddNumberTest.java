fcbec389e5a757c00bbd0bfb6b0abe5019b9bcdc
Ustinova <a.s.ustinowa@gmail.com>
2023-09-20 23:21:22 +0400

    Homework_3

--- unit-tests-3 (Код семинара)/Test/hw3/OddNumberTest.java
+++ unit-tests-3 (Код семинара)/Test/hw3/OddNumberTest.java
@@ -27,6 +27,28 @@ public class OddNumberTest {
         Assertions.assertFalse(evenodd.evenOddNumber(3));
     }
 
+    //Тесты ко второму заданию
+    @Test
+    void testInRange(){
+        MainHW numintervel = new MainHW();
+        Assertions.assertTrue(numintervel.numberInInterval(25));
+    }
 
+    @Test
+    void testInRangeMore(){
+        MainHW numintervel = new MainHW();
+        Assertions.assertTrue(numintervel.numberInInterval(100));
+    }
 
+    @Test
+    void testOutOfRange(){
+        MainHW numintervel = new MainHW();
+        Assertions.assertFalse(numintervel.numberInInterval(24));
+    }
+
+    @Test
+    void testOutOfRangeMore(){
+        MainHW numintervel = new MainHW();
+        Assertions.assertFalse(numintervel.numberInInterval(101));
+    }
 }
--- unit-tests-3 (Код семинара)/src/seminars/third/hw/MainHW.java
+++ unit-tests-3 (Код семинара)/src/seminars/third/hw/MainHW.java
@@ -13,5 +13,12 @@ public class MainHW {
 
     // HW 3.2. Нужно написать метод который проверяет, попадает ли переданное число в интервал (25;100) и возвращает true, если попадает и false - если нет,
     // покрыть тестами метод на 100%
+    public boolean numberInInterval(int n) {
+        if ((n >= 25) && (n <= 100)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 
 }
